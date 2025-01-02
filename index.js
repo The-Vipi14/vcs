50a0fde3b88639fec28f2ed64be72464951afc5b
Vipin <vipinbhardwal286@gmail.com>
2025-01-02 19:40:26 +0530

    Third branch created

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const name2 = "vipin"
 function getName(){
-    console.log(`Your name is the ${name2}`)
+    console.log(`Your name is the ${name2}`);
     return name1;
 } 
 
+function forThirdBranch(){
+    console.log("Message from third branch");
+}
 
 
