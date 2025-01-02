d32b149ff480b7026eb06b38e2271eb0ef508efd
Vipin <vipinbhardwal286@gmail.com>
2025-01-02 15:28:44 +0530

    new branch

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const name2 = "vipin"
+const newUser = "Om"
 function getName(){
     console.log(`Your name is the ${name2}`)
+    // now we have created new branch named as My-Branch
+    
+    console.log(`This is new branch for ne uesr ${newUser}`)
     return name1;
 } 
 
